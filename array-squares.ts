0defc64866442fdc41aaea6e11dd665d2d42ad40
unknown <ryan.piinheiro@hotmail.com>
2025-10-07 11:32:37 -0300

    feat: implementa programa que ordena de forma decrescente

--- array-squares.ts
+++ array-squares.ts
@@ -21,25 +21,40 @@ function calculaQuadradoForEach(numbers: number[]): number[] {
 
 // Concatena strings
 function concatenaStringsComJoin(strings: string[]): string {
-
   return strings.join(" ");
 }
 
+// Ordena strings em ordem decrescente
+function ordenaStringDecrescente(strings: string[]): string[] {
+
+  return strings.sort((a: string, b: string) => b.localeCompare(a));
+}
 
 function main() {
-//   const testArray = [3, 5, 7, 3, 8, 9, 1];
+  //   const testArray = [3, 5, 7, 3, 8, 9, 1];
 
-//   console.log("Array original:", testArray);
-//   console.log("Quadrados (for simples):", calculaQuadrado(testArray));
-//   console.log("Quadrados (forEach):", calculaQuadradoForEach(testArray));
+  //   console.log("Array original:", testArray);
+  //   console.log("Quadrados (for simples):", calculaQuadrado(testArray));
+  //   console.log("Quadrados (forEach):", calculaQuadradoForEach(testArray));
 
   // Concatena strings
-  const testArrayStrings = ["Arrays", "com", "TypeScript"];
+//   const testArrayStrings = ["Arrays", "com", "TypeScript"];
 
-  console.log("Array original:", testArrayStrings);
-  console.log("Concatenação com join:", concatenaStringsComJoin(testArrayStrings));
+//   console.log("Array original:", testArrayStrings);
+//   console.log(
+//     "Concatenação com join:",
+//     concatenaStringsComJoin(testArrayStrings)
+//   );
 
+  // Ordena strings em ordem decrescente
+  const testArrayOrdenacao = ["carro", "boneco", "ave", "lapis"];
 
+  console.log("\n--- Ordenação Decrescente ---");
+  console.log("Array original:", testArrayOrdenacao);
+  console.log(
+    "Array ordenado decrescente:",
+    ordenaStringDecrescente(testArrayOrdenacao)
+  );
 }
 
 // Executar se o arquivo for chamado diretamente
